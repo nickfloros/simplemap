32b8e275a9ee3e98d3916a84dd1e5516a437e774
nickfloros <nickfloros@gmail.com>
2014-01-15 23:44:54 +0000

    replicate example from dartjs site

--- web/ptestmap2.dart
+++ web/ptestmap2.dart
@@ -1,16 +1,32 @@
-library ptestmap2;
+library google_maps;
 
-import 'dart:html';
-import 'package:google_maps/google_maps.dart';
+// This code is derived from
+// https://developers.google.com/maps/documentation/javascript/tutorial#HelloWorld
+// You can view the original JavaScript example at
+// https://developers.google.com/maps/documentation/javascript/examples/map-simple
+
+import 'dart:html' show querySelector;
+import 'dart:js' show context, JsObject;
 
 void main() {
-  visualRefresh = true;
+  // The top-level getter context provides a JsObject that represents the global
+  // object in JavaScript.
+  final google_maps = context['google']['maps'];
 
-  final mapOptions = new MapOptions()
-    ..zoom = 8
-    ..center = new LatLng(-34.397, 150.644)
-    ..mapTypeId = MapTypeId.ROADMAP
-    ;
-  
-  final map = new GMap(querySelector("#map_canvas"), mapOptions);
+  // new JsObject() constructs a new JavaScript object and returns a proxy
+  // to it.
+  var center = new JsObject(google_maps['LatLng'], [-34.397, 150.644]);
+
+  var mapTypeId = google_maps['MapTypeId']['ROADMAP'];
+
+  // new JsObject.jsify() recursively converts a collection of Dart objects
+  // to a collection of JavaScript objects and returns a proxy to it.
+  var mapOptions = new JsObject.jsify({
+      "center": center,
+      "zoom": 8,
+      "mapTypeId": mapTypeId
+  });
+
+  // Nodes are passed though, or transferred, not proxied.
+  new JsObject(google_maps['Map'], [querySelector('#map-canvas'), mapOptions]);
 }
